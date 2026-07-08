91b45e3987c0a1b393e38098d26182e84d2d41ad
Shambhavi Verma <shambhaviver19@gmail.com>
2026-07-08 22:07:34 +0530

    Debugging the deployment bug

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,45 +1,62 @@
 const nodemailer = require("nodemailer");
 
+
 const sendEmail = async (options) => {
+
   try {
+
     console.log("Preparing email...");
-    console.log("EMAIL:", process.env.EMAIL_USER);
     console.log("Sending mail to:", options.email);
 
+
     const transporter = nodemailer.createTransport({
+
       host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+
+      port: 587,
+
+      secure: false,
+
+      family: 4, // force IPv4
+
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      connectionTimeout: 10000,
+
     });
+
 
     console.log("Before sendMail");
 
+
     const info = await transporter.sendMail({
-      from: `"E-Commerce Support" <${process.env.EMAIL_USER}>`,
+
+      from: `"HelloMaam" <${process.env.EMAIL_USER}>`,
+
       to: options.email,
+
       subject: options.subject,
+
       text: options.message,
+
     });
+
+
 
     console.log("Mail sent:", info.messageId);
 
-    return true;
+
   } catch (error) {
 
-  console.log("========= EMAIL ERROR =========");
 
-  console.log(error);
+    console.log("Email Error:", error);
 
-  console.log("===============================");
+    throw error;
 
-  throw error;
+  }
 
-}
 };
+
 
 module.exports = sendEmail;
